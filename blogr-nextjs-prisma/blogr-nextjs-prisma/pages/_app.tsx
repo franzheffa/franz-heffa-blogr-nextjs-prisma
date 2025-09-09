8f7c08091fdeef9cf64660dfd21d38fec471027c
franz heffa <franzheffa@buttertech.io>
2025-09-09 05:29:56 -0400

    Update _app.tsx

--- blogr-nextjs-prisma/blogr-nextjs-prisma/pages/_app.tsx
+++ blogr-nextjs-prisma/blogr-nextjs-prisma/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import '../components/styles.css';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={(pageProps as any).session}>
