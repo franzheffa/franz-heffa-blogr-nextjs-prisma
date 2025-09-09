06e892994b53c72fce0683af8bba2b864f510b4f
franz heffa <franzheffa@buttertech.io>
2025-09-09 04:43:09 -0400

    build: final clean slate and production deployment

--- blogr-nextjs-prisma/blogr-nextjs-prisma/pages/api/auth/[...nextauth].ts
+++ blogr-nextjs-prisma/blogr-nextjs-prisma/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '../../../lib/prisma';
-
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
