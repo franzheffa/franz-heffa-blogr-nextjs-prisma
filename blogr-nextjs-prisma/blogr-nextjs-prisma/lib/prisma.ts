4d702ef0d14975ee9e2414482c35f0108aeae6c1
franz heffa <franzheffa@buttertech.io>
2025-09-12 04:36:05 -0400

    build: final production-ready state with initial migration

--- blogr-nextjs-prisma/blogr-nextjs-prisma/lib/prisma.ts
+++ blogr-nextjs-prisma/blogr-nextjs-prisma/lib/prisma.ts
@@ -1,15 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-const prisma =
-  global.prisma ||
-  new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 export default prisma;
